3627da239193cba8f489359d6d6aa6ec65eb7f57
Atharv Kolekar <126802492+atharvkolekar190@users.noreply.github.com>
2026-02-05 12:39:15 +0530

    Add commit message print statement to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
   public static void main(String[] args){
     System.out.println("Hello! Atharv this side....... ");
+    System.out.println("Commiting New change on git");
   }
   
 }
