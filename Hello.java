33d632cde52ae3f31c2d69cf696c4d657f4a0b22
Atharv Kolekar <126802492+atharvkolekar190@users.noreply.github.com>
2026-02-09 11:36:21 +0530

    Add a new Polling msg

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello! Atharv this side....... ");
     System.out.println("Commiting New change on git");
+    System.out.println("After Chnaging the POLL SCM as * * * * *");
   }
   
 }
